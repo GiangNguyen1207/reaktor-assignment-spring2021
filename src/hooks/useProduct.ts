9f12ecafbbd55ef7577ee3ea2a20c74143efe976
giang <nguyengiang.nchg@gmail.com>
2020-11-13 11:19:04 +0200

    fix search feature

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import _orderBy from 'lodash/orderBy'
 
@@ -7,8 +7,10 @@ import { getProducts } from 'redux/actions'
 import Categories from 'constants/Categories'
 import { Product } from 'redux/type'
 
-export default function useProduct(category: string) {
+export default function useProduct(category: string, input: string) {
   const dispatch = useDispatch()
+  const [productList, setProductList] = useState<Product[]>([])
+  const [searchedResults, setSearchResults] = useState<Product[]>([])
   const { pJackets, pShirts, pAccessories } = Categories
   const { jackets, shirts, accessories, availability } = useSelector(
     (state: RootState) => ({
@@ -27,23 +29,34 @@ export default function useProduct(category: string) {
     ) {
       dispatch(getProducts(category))
     }
-  }, [dispatch, category])
 
-  const sortedProducts: Product[] = _orderBy(
-    category === pJackets
-      ? jackets
-      : category === pShirts
-      ? shirts
-      : accessories,
-    ['name'],
-    ['asc']
-  )
+    const sortedProducts: Product[] = _orderBy(
+      category === pJackets
+        ? jackets
+        : category === pShirts
+        ? shirts
+        : accessories,
+      ['name'],
+      ['asc']
+    )
+
+    if (input) {
+      let searchedProducts: Product[] = []
+      searchedProducts = sortedProducts.filter((p) =>
+        p.name.toLowerCase().includes(input.toLowerCase())
+      )
+      setSearchResults(searchedProducts)
+    } else {
+      setProductList(sortedProducts)
+    }
+  }, [dispatch, category, input, jackets, shirts, accessories])
 
   return {
     jackets,
     shirts,
     accessories,
     availability,
-    sortedProducts,
+    productList,
+    searchedResults,
   }
 }
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -4,15 +4,16 @@ import Header from 'components/Header'
 import NavigationBar from 'components/NavigationBar'
 import useProduct from 'hooks/useProduct'
 import DisplayTable from 'components/DisplayTable'
-import useSearchProduct from 'hooks/useSearchProduct'
 import { Product } from 'redux/type'
 
 const DisplayPage = () => {
   const [input, setInput] = useState<string>('')
   const [searchedProducts, setSearchProducts] = useState<Product[]>([])
   const category = location.pathname.substr(1)
-  const { productList } = useSearchProduct(category, input)
-  const { availability, sortedProducts } = useProduct(category)
+  const { availability, productList, searchedResults } = useProduct(
+    category,
+    input
+  )
 
   const tHeaders = useMemo(
     () => ['name', 'color', 'price', 'manufacturer', 'availability'],
@@ -27,7 +28,7 @@ const DisplayPage = () => {
   )
 
   const handleClick = () => {
-    setSearchProducts(productList)
+    setSearchProducts(searchedResults)
   }
 
   return (
@@ -36,8 +37,8 @@ const DisplayPage = () => {
       <NavigationBar />
       <DisplayTable
         tHeaders={tHeaders}
-        productList={searchedProducts}
-        sortedProducts={sortedProducts}
+        productList={productList}
+        searchedProducts={searchedProducts}
         availability={availability}
       />
     </>
@@ -45,3 +46,38 @@ const DisplayPage = () => {
 }
 
 export default React.memo(DisplayPage)
+
+// let searchedProducts: Product[] = []
+// if (input) {
+//   if (category === pJackets) {
+//     searchedProducts = jackets.filter((p) =>
+//       p.name.toLowerCase().includes(input.toLowerCase())
+//     )
+//   } else if (category === pShirts) {
+//     searchedProducts = shirts.filter((p) =>
+//       p.name.toLowerCase().includes(input.toLowerCase())
+//     )
+//   } else {
+//     searchedProducts = accessories.filter((p) =>
+//       p.name.toLowerCase().includes(input.toLowerCase())
+//     )
+//   }
+// }
+// console.log(searchedProducts)
+
+// const sortedProducts: Product[] = _orderBy(
+//   category === pJackets
+//     ? _isEmpty(searchedProducts)
+//       ? jackets
+//       : searchedProducts
+//     : category === pShirts
+//     ? _isEmpty(searchedProducts)
+//       ? shirts
+//       : searchedProducts
+//     : _isEmpty(searchedProducts)
+//     ? accessories
+//     : searchedProducts,
+//   ['name'],
+//   ['asc']
+// )
+// setProductList(sortedProducts)
